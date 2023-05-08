4dfcff543bcd4af35c94d79bdc9b46944c3828a8
Leica <leica.shen@gmail.com>
2023-05-07 20:03:08 -0700

    part2.1 finish

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,58 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  
 }
+
+const synth = window.speechSynthesis;
+const but = document.querySelector("button");
+const inputTxt = document.querySelector("textarea");
+const voiceSelect = document.querySelector("select");
+let voices = [];
+
+let image = document.querySelector("img");
+
+function populateVoiceList() {
+  voices = synth.getVoices();
+
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement("option");
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+    if (voices[i].default) {
+      option.textContent += " — DEFAULT";
+    }
+
+    option.setAttribute("data-lang", voices[i].lang);
+    option.setAttribute("data-name", voices[i].name);
+    voiceSelect.appendChild(option);
+  }
+}
+
+populateVoiceList();
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
+}
+
+but.onclick = (event) => {
+  event.preventDefault();
+
+  const utterThis = new SpeechSynthesisUtterance(inputTxt.value);
+  const selectedOption =
+    voiceSelect.selectedOptions[0].getAttribute("data-name");
+  for (let i = 0; i < voices.length; i++) {
+    if (voices[i].name === selectedOption) {
+      utterThis.voice = voices[i];
+    }
+  }
+
+  utterThis.onstart = function() {
+    image.src = "assets/images/smiling-open.png";
+  };
+
+  utterThis.onend = function() {
+    image.src = "assets/images/smiling.png";
+  };
+
+  synth.speak(utterThis);
+};
